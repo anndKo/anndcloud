6d30bc38da9fec192780896561a899bc5b0e2c13
Hnuecst <email_dung_cua_Hnuecst>
2026-01-14 18:05:53 +0700

    Fix 404 error using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// 1. Thêm dòng này để import HashRouter
-import { HashRouter } from "react-router-dom"; 
+// 1. XÓA dòng import HashRouter (vì không dùng ở đây nữa)
+// import { HashRouter } from "react-router-dom"; 
 
 createRoot(document.getElementById("root")!).render(
-  // 2. Bọc component App bên trong HashRouter
-  <HashRouter>
-    <App />
-  </HashRouter>
+  // 2. XÓA thẻ <HashRouter> bao quanh, CHỈ ĐỂ LẠI <App />
+  <App />
 );
